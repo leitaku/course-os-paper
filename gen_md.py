807256c0fd7cf9c9fc5e73a551683b1c27ce4c18
Yixing Lao <linuxthink@gmail.com>
2016-03-09 20:53:11 -0800

    add scripts to generate md files

---
+++ gen_md.py
@@ -0,0 +1,43 @@
+names = [
+    '01_07_01_the_multiprogramming',
+    '01_07_02_nucleus',
+    '01_12_01_tenex',
+    '01_12_02_hydra',
+    '01_14_01_protection',
+    '01_14_02_multics',
+    '01_14_03_multics_virutal_memory',
+    '01_19_01_unix_timeshare',
+    '01_19_02_plan_9_bell_labs',
+    '01_26_01_pilot',
+    '01_26_02_rethinking_software_stack',
+    '01_28_01_monitors_p549-hoare',
+    '01_28_02_mesa_p105-lampson',
+    '02_02_01_virtual_memory_01653971',
+    '02_02_02_machine_independent_vm_p31-rashid',
+    '02_04_01_v_kernel_p129-cheriton',
+    '02_04_02_sprite-ieee-computer88',
+    '02_09_02_p201-feeley',
+    '02_11_01_micro_kernel_p66-hartig',
+    '02_11_02_exokernel_p251-engler',
+    '02_16_01_xen_p164-barham',
+    '02_16_12_cell_p173-andrus',
+    '02_18_01_scheduler_p95-anderson',
+    '02_18_02_lottery_waldspurger',
+    '02_23_02_osdi14-paper-belay',
+    '02_25_01_p181-mckusick',
+    '02_25_02_p1-rosenblum',
+    '03_01_01_softupdates',
+    '03_01_02_rio_p74-chen',
+    '03_03_01_gfs_p29-ghemawat',
+    '03_08_01_many_cores_boyd-wickizer',
+    '03_08_02_multikernel_baumann-sosp09',
+    '03_10_01_potemkin-sosp05',
+    '03_10_02_tdf-nsdi06',
+    '03_10_03_diecast-nsdi08',
+    '03_10_04_diff-engine-osdi08',
+    '03_10_05_bluesky-fast12'
+]
+
+for name in names:
+    f = open(name + '.md', 'w+')
+    f.close()
